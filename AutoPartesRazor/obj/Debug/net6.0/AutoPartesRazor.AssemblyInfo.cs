2c503b63487ddd2980a8e0ec0f18160ac3f10dbf
Sebastian <sebastevez91@gmail.com>
2025-10-12 11:43:27 -0300

    Add background color

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e188e9f6d4ce3ad1eb45a7d44d9534989ee2db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0860142df37d97b4ca011d6aecbc5222a29f0ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
