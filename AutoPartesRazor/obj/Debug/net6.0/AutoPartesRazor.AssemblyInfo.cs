579b4b0620e2860e2ddf0311222adec268767fe4
Sebastian <sebastevez91@gmail.com>
2025-11-11 10:09:15 -0300

    Changes en local

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43766417f2a5079260c8bdb8abb1b49eac5a2b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ba53c89eb30206586cd48551cb25c18634f910")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
