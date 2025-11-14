eb07016476d3c82de5370a5da517f1516b54ecd6
Sebastian <sebastevez91@gmail.com>
2025-11-14 19:08:20 -0300

    Traer cambios pedidos

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125c1afec5ee3850816671d7ed2d74e8b6c7bdd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fddce76d77e932a846bf903a35b2529ee3b3f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
