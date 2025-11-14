e8e2f288499902272b543ef5d4db6689057f48cc
Benjamin <bg42477@gmail.com>
2025-11-14 18:26:27 -0300

    Actualizacion de Ordenes y Pedidos

---
+++ AutoPartesRazor/Migrations/20251114011755_AddFeedbackToOrder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoPartesRazor.Migrations
+{
+    public partial class AddFeedbackToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Calificacion",
+                table: "Order",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Feedback",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calificacion",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "Feedback",
+                table: "Order");
+        }
+    }
+}
