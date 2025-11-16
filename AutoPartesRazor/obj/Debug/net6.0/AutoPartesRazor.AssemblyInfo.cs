9a9d98e39a8bf746d4d1df9aea9562dd24f9331c
Sebastian <sebastevez91@gmail.com>
2025-11-16 19:40:26 -0300

    changes in error data

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90fbe2e817d25cca6d015b27a91bc3f03361e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1610f3910f7cf1a85286e9dfe207ab3530f9ad56")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
