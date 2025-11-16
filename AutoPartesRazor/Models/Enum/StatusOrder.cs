f6bca4c92b7acf32bd63693df55574ff7c3f1ff9
Sebastian <sebastevez91@gmail.com>
2025-11-15 23:08:49 -0300

    Changes in model Supplier

---
+++ AutoPartesRazor/Models/Enum/StatusOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPartesRazor.Models.Enum;
+
+public enum StatusOrder
+{
+    Pendiente,
+    Enviado,
+    Entregado,
+    Cancelado,
+    Completado
+}
--- AutoPartesRazor/Models/PurchaseOrder.cs
+++ AutoPartesRazor/Models/PurchaseOrder.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AutoPartesRazor.Models.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace AutoPartesRazor.Models;
 
@@ -7,17 +8,26 @@ public class PurchaseOrder
 {
     public int Id { get; set; }
 
+    // Forenig key Producto
     [Required]
     public int ProductId { get; set; }
     public Product? Product { get; set; }
 
+    // Forening Key Proveédor
     [Required]
     public int SupplierId { get; set; }
+
+    [Display(Name ="Provéedor")]
     public Supplier? Supplier { get; set; }
 
     [Required]
     [Range(1, 9999)]
+    [Display(Name = "Cantidad")]
     public int Quantity { get; set; }
 
+    [Display(Name ="Estado")]
+    public StatusOrder? Status { get; set; } = StatusOrder.Pendiente;
+
+    [Display(Name ="Fecha de creación")]
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
