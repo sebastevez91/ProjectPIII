13c478c5f8a82863b116cd07e6280b08699859af
Sebastian <sebastevez91@gmail.com>
2025-11-19 21:42:59 -0300

    Create pages roles and permisso

---
+++ AutoPartesRazor/ViewModels/ManageRolePermissionsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoPartesRazor.ViewModels;
+
+public class ManageRolePermissionsViewModel
+{
+    public string RoleId { get; set; }
+    public string RoleName { get; set; }
+    public List<PermissionGroupViewModel> PermissionGroups { get; set; }
+}
---
+++ AutoPartesRazor/ViewModels/RoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPartesRazor.ViewModels;
+
+public class RoleViewModel
+{
+    public string Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre del rol es obligatorio")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres")]
+    [Display(Name = "Nombre del Rol")]
+    public string Name { get; set; }
+
+    [Display(Name = "Número de usuarios")]
+    public int UsersCount { get; set; }
+
+    [Display(Name = "Número de permisos")]
+    public int PermissionsCount { get; set; }
+}
