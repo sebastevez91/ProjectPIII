54e076f0115792c3efd1df4baea899b5b9d9b8b3
sebatevez91 <Sebastevez91@gmail.com>
2025-11-22 23:57:51 -0300

    file never

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2218238723f8b0bf98eb59fa1c9f9364cab4e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d4084ad0c26e16bfa8c241e5e7001a44e34bab")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
