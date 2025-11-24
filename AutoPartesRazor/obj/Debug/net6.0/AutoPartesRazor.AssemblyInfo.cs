14fb1a3a71cd29dbda8a98c43b681060c5634aa6
EmanuelFeijoo <manugonzalofeijoo@gmail.com>
2025-11-24 01:58:21 -0300

    Guardando cambios antes de cambiar de rama

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148d9f3f31e9633045717122a958ecfa1e932c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c968c799d1273b6064b0d47bc19199e0f4594cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
