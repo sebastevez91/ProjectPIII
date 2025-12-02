2bf7f9b52b9e116e90af074b7d82e9b28e2503a0
sebatevez91 <Sebastevez91@gmail.com>
2025-12-01 22:32:16 -0300

    after modified

---
+++ AutoPartesRazor/Models/OrderEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoPartesRazor.Models;
+
+public class OrderEvent
+{
+    [Key]
+    public int Id { get; set; }
+
+    // Clave foránea al pedido
+    public int OrderId { get; set; }
+    [ForeignKey("OrderId")]
+    public Order? Order { get; set; }
+
+    // Estado del pedido en el momento del evento
+    [Required]
+    [StringLength(50)]
+    public string Status { get; set; } = string.Empty;
+
+    // Mensaje detallado para mostrar en la línea de tiempo
+    [Required]
+    [StringLength(255)]
+    public string Description { get; set; } = string.Empty;
+
+    // Fecha y hora exacta del evento
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+
+    // Opcional: Para detalles como Nro. de Tracking, nombre del empleado, etc.
+    public string? Reference { get; set; }
+}
--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee14bb1d6893f71620e9cdb8035008377cf1012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1301415088b4fe89fc982f4d2646d32fa36e0a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
