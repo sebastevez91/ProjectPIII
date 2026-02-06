21cd75d55ed842bf310e89d67b23bff52f23d3b7
sebatevez91 <Sebastevez91@gmail.com>
2026-02-06 15:16:42 -0300

    finish project

--- AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
+++ AutoPartesRazor/obj/Debug/net6.0/AutoPartesRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03ce0c26d8ed1503036119997e1c75c309b5c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d9090b9c4b14398ff0df5d6a29ca796ebb2faa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoPartesRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
